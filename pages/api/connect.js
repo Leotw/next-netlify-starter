31f8fe192c0ef1c213808e88dcf58808f527daf4
selmaliu <1123978215@qq.com>
2025-07-02 14:20:05 +0800

    feat

--- pages/api/connect.js
+++ pages/api/connect.js
@@ -2,6 +2,7 @@ import { runCommands } from '../util';
 
 export default async function handler(req, res) {
   // connect();
+
   const commandRet = await runCommands();
   const textRet = commandRet.join("\n");
   const result = textRet.split('\n').filter(e => e);
